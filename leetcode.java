04e3fd0ef4d85d50cc37aa3bb5b75b21dbb2639a
Varun Kharkwal <varunkh12345@gmail.com>
2024-04-16 14:14:30 +0530

    Add Java code for adding a row to a binary tree

--- leetcode.java
+++ leetcode.java
@@ -1,26 +1,51 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 class Solution {
-  public boolean isIsomorphic(String s, String t) {
-    int[] hash = new int[256];
-    for (int i = 0; i < s.length(); i++) {
-      char sChar = s.charAt(i);
-      if (hash[sChar] == 1) continue;
-      hash[sChar] = 1;
-      char tChar = t.charAt(i);
-      for (int j = i; j < s.length(); j++) {
-        if (s.charAt(j) == sChar || t.charAt(j) == tChar) {
-          if (s.charAt(j) == sChar && t.charAt(j) == tChar) continue;
-          else return false;
-        }
+  public TreeNode addOneRow(TreeNode root, int val, int depth) {
+    if (depth == 1) return new TreeNode(val, root, null);
+    if (depth == 2) {
+      root.left = new TreeNode(val, root.left, null);
+      root.right = new TreeNode(val, null, root.right);
+      return root;
+    }
+    Queue<TreeNode> queue = new LinkedList();
+    if (root.left != null) queue.add(root.left);
+    if (root.right != null) queue.add(root.right);
+    int n;
+    for (int i = 2; i < depth-1; i++) {
+      n = queue.size();
+      for (int j = 0; j < n; j++) {
+        if (queue.peek().left != null) queue.add(queue.peek().left);
+        if (queue.peek().right != null) queue.add(queue.peek().right);
+        queue.poll();
       }
     }
-    return true;
+    for (TreeNode node : queue) {
+      node.left = new TreeNode(val, node.left, null);
+      node.right = new TreeNode(val, null, node.right);
+    }
+    return root;
   }
 }
+
 public class leetcode {
   public static void main(String[] args) {
     Solution s = new Solution();
-    System.out.println(s.isIsomorphic("egg", "add"));
-    System.out.println(s.isIsomorphic("foo", "bar"));
-    System.out.println(s.isIsomorphic("paper", "title"));
+    TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(4), null), new TreeNode(3));
+    TreeNode Rroot = s.addOneRow(root, 5, 4);
   }
 }
